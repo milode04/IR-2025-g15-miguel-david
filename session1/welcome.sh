aab4da095b0ebfdc014d92885900986edcbe502a
milode04 <miguellopdelgado@gmail.com>
2025-09-23 16:46:29 +0200

    Add bye world! to README.md

---
+++ session1/welcome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello my name is Miguel and I like football"
+echo "Group members:"
+echo " Miguel Lopez Delgado"
+echo "David Pujol Ortega"
+echo "Here the full path"
+pwd
+ls -l
+
